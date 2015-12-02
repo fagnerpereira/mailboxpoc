9d9f9e117b7a877bb21984de4aad8084b2b97ab3
Fagner Pereira <fagnerfpr@gmail.com>
2015-12-02 17:30:46 -0200

    fixing route

--- app/controllers/mailboxes_controller.rb
+++ app/controllers/mailboxes_controller.rb
@@ -1,20 +1,12 @@
 class MailboxesController < ApplicationController
-
-#  def create
-#    data = Multimap.new
-#    data[:priority] = 0
-#    data[:description] = "Sample route"
-#    data[:expression] = "match_recipient('.*@sandbox4176cdb3671d4dbb824bdcf7a2691fb8.mailgun.org')"
-#    data[:action] = "forward('http://requestb.in/1eikb3f1')"
-#    data[:action] = "stop()"
-#    RestClient.post "https://api:key-dc38bf9d560402dc6e6e99012a6824fa"\
-#    "@api.mailgun.net/v3/routes", data
-#  end
-
+  skip_before_filter :verify_authenticity_token
 
   def messages
-    user = User.find_by_mailgun!(params[:recipient])
+    email = params[:message][:headers][:to]
+    user = User.find_by_mailgun!(email)
     user.received = true
+    render nothing: true, status: :ok
   rescue
+    render nothing: true, status: :not_found
   end
 end
